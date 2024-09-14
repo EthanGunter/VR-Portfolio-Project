ab5e0429c6efc94745fd3be67d86618c6b63f371
Ethan Gunter <egunter.tech@gmail.com>
2024-09-13 23:15:13 -0400

    Abolished questionable SpawnZones
    All they did was expose a function that set the position of an object within the zone, but zones already provide a GetRandomPoint() function, and entities should handle their own spawn in animation, not just pop into existence...

--- Assets/Code/Entities/LevelDesign/SpawnZoneCollection.cs
+++
@@ -1,8 +0,0 @@
-using SolarStorm.UnityToolkit;
-using UnityEngine;
-
-namespace SolarStorm.LevelDesign
-{
-    [CreateAssetMenu(menuName = "Shared Data/Spawn Zone Set")]
-    public class SpawnZoneCollection : RuntimeSet<SpawnZone> { }
-}
